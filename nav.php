892bac90c038773e19d97b6ae9f6b128c16a1c04
dilaw <154446517+siopaoboy@users.noreply.github.com>
2024-08-02 18:39:54 -0500

    added a navigation bar for the site that will direct them to the pages available

---
+++ nav.php
@@ -0,0 +1,9 @@
+	<nav>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="menu.php">Menu</a></li>
+			<li><a href="order.php">Order</a></li>
+			<li><a href="about.php">About Us</a></li>
+			<li><a href="careers.php">Careers</a></li>
+		</ul>
+	</nav>
