8305f0e7d6eb63f61691f2dc5575faf459e02f55
dilaw <154446517+siopaoboy@users.noreply.github.com>
2024-08-08 18:41:45 -0500

    added register.php, used to help the user register for an account on the site

---
+++ register.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Register Now!</title>
+</head>
+<body>
+	<h1>Become a BOBAPEGGER Today!</h1>
+
+	<form method="POST" action="register.php">
+		<label for="usernameRegister">Username:</label>
+		<input type="text" name="usernameRegister"> <br> <br>
+		<label for="passwordRegister">Password:</label>
+		<input type="text" name="passwordRegister"> <br> <br>
+		<button name="register" type="submit">Register</button>
+	</form>
+
+</body>
+</html>
