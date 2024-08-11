1418333b185213c8331b02f4c98ab7855f77888e
dilaw <154446517+siopaoboy@users.noreply.github.com>
2024-08-11 02:09:35 -0500

    added stylesheets for each php file (nothing has been added into the stylesheets yet)

--- reviews.php
+++ reviews.php
@@ -23,13 +23,14 @@
 <!DOCTYPE html>
 <html>
 <head>
+
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="styles.css">
+
+	<link rel="stylesheet" type="text/css" href="reviews.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 	<title>Reviews ~ BOBAPEG</title>
-	<h1>BOBAPEG</h1>
 	<?= include('nav.php') ?>
 </head>
 <body class="review">
