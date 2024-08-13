d69cd996ecb7e5328530e5ab6e01669c8ad0010b
dilaw <154446517+siopaoboy@users.noreply.github.com>
2024-08-12 19:00:24 -0500

    replaced careers page with news page

---
+++ news.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>News ~ BOBAPEG</title>
+	<?= include('nav.php'); ?> <br>
+</head>
+<body>
+	<h1><u>News</u></h1> <br>
+
+</body>
+</html>
